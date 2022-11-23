68c33bfded3de1bfe3da8244067faaf729f1f326
Rasmus <rasmus.skovhaven@gmail.com>
2022-11-23 16:03:05 +0100

    Added merged array for customers and projects

--- components/partials/Sidebar.vue
+++ components/partials/Sidebar.vue
@@ -3,6 +3,23 @@ import { useCustomerStore } from "~/store/customers";
 import { useEstimateSheetStore } from "~~/store/estimateSheets";
 const customerStore = useCustomerStore();
 const sheetStore = useEstimateSheetStore();
+
+let customers = ref([]);
+
+watchEffect(() => {
+  /* Merge customers and projects */
+  if (customerStore.GET_CUSTOMERS && sheetStore.getEstimateSheets) {
+    customerStore.GET_CUSTOMERS.map((c) => {
+      sheetStore.GET_ESTIMATE_SHEETS.filter((s) => {
+        s.customerId === c.id
+          ? customers.value.push({ ...c, sheets: s })
+          : customers.value.push({ ...c });
+      });
+    });
+
+    console.log(customers.value);
+  }
+});
 </script>
 
 <template>
