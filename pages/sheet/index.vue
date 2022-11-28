b8bd3eeeff0168f0ea6ecc21ca93c6bd72a39efc
mich5c18 <mich5c18@edu.ucl.dk>
2022-11-28 09:52:26 +0100

    Endpoint/Store Error on get

--- pages/sheet/index.vue
+++ pages/sheet/index.vue
@@ -1,22 +1,33 @@
 <script setup>
-const router = useRouter();
+import { useAuthStore } from "~/store/auth";
+import { useEpicStore } from "~/store/epics";
+import { useEpicStatusStore } from "~/store/epicstatus";
 
-function navigate() {
-  return navigateTo({
-    path: "/sheet",
-    query: {
-      id: 1,
-    },
-  });
-}
+const authStore = useAuthStore();
+const epicStore = useEpicStore();
+const epicStatusStore = useEpicStatusStore();
 
-onMounted(() => {
-  return navigate();
+const postData = reactive({
+  epic: {
+    name: "Epic navn"
+    
+  },
 });
+
+let status = [] 
+
+onMounted( async () => {
+    await epicStatusStore.getEpicStatus(authStore.API_TOKEN);
+    console.log("Index", epicStore.EPIC_STATUS);
+})
+
+const GetStatus = async () => {
+    await epicStatusStore.getEpicStatus(authStore.API_TOKEN);
+    console.log("Index", epicStore.EPIC_STATUS);
+};
+
 </script>
 
 <template>
-  <div>
-    {{ $route }}
-  </div>
+        <Button @click="CreateEpic(authStore.API_TOKEN, postData.epic)"></Button>
 </template>
---
+++ store/epicStatus.js
@@ -0,0 +1,61 @@
+import { defineStore } from "pinia";
+
+export const useEpicStatusStore = defineStore("epic-status-store", () => {
+    /* State */
+    const epicStatus = ref([]);
+
+    /* Actions */
+    const setEpicStatus = (payload) => {
+        epicStatus.value = payload;
+        console.log("payload", payload)
+    };
+
+    const getEpicStatus = async (token) => {
+        if (!token) return [];
+        const runtimeConfig = useRuntimeConfig();
+
+        const response = await $fetch(
+            `${runtimeConfig.public.API_URL}/epicstatus`,
+            {
+                method: "GET",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+        console.log("response", response)
+        setEpicStatus(response);
+    };
+
+    const createEpicStatus = async (token, obj) => {
+        if (!token) return [];
+        const runtimeConfig = useRuntimeConfig();
+
+        const response = await $fetch(
+            `${runtimeConfig.public.API_URL}/epicstatus`,
+            {
+                method: "POST",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+                body: obj,
+            }
+        );
+
+        console.log("post epic res", response);
+    };
+
+    /* Getters */
+    const EPIC_STATUS = computed(() => epicStatus.value);
+
+    return {
+        getEpicStatus,
+        createEpicStatus,
+        EPIC_STATUS
+    };
+
+});
