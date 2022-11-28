e7263e2571dec9476a3856d06cd13d8ba99714bf
mich5c18 <mich5c18@edu.ucl.dk>
2022-11-28 12:31:13 +0100

    Can create an epic

--- pages/sheet/index.vue
+++ pages/sheet/index.vue
@@ -2,32 +2,41 @@
 import { useAuthStore } from "~/store/auth";
 import { useEpicStore } from "~/store/epics";
 import { useEpicStatusStore } from "~/store/epicstatus";
+import { useEstimateSheetStore } from "~/store/estimatesheets";
 
 const authStore = useAuthStore();
 const epicStore = useEpicStore();
 const epicStatusStore = useEpicStatusStore();
+const estimateSheetStore = useEstimateSheetStore();
 
 const postData = reactive({
   epic: {
-    name: "Epic navn"
-    
+    name: "Epic navn",
+    estimateSheetId: null,
+    epicStatusId: null
   },
 });
 
-let status = [] 
-
 onMounted( async () => {
-    await epicStatusStore.getEpicStatus(authStore.API_TOKEN);
-    console.log("Index", epicStore.EPIC_STATUS);
+  await epicStatusStore.getEpicStatus(authStore.API_TOKEN);
+  await estimateSheetStore.getEstimateSheets(authStore.API_TOKEN);
+
+  postData.epic.estimateSheetId = estimateSheetStore.ESTIMATE_SHEETS[0].id;
+  //postData.epic.epicStatusId = epicStatusStore.EPIC_STATUS[0].id;
+
+  epicStatusStore.EPIC_STATUS.forEach(element => {
+    //console.log(element.default);
+    if (element.default) {
+      postData.epicStatusId = element.id;
+    }
+  });
 })
 
-const GetStatus = async () => {
-    await epicStatusStore.getEpicStatus(authStore.API_TOKEN);
-    console.log("Index", epicStore.EPIC_STATUS);
-};
+
 
 </script>
 
 <template>
-        <Button @click="CreateEpic(authStore.API_TOKEN, postData.epic)"></Button>
+        <Button text="Click mig for fanden" @click="epicStore.createEpic(authStore.API_TOKEN, postData.epic)"></Button>
+        <p v-for="status in epicStatusStore.EPIC_STATUS">{{status}}</p>
 </template>
