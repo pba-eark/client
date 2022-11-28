2e5814df5edcbaa2d0be1a8266a36b6c089393b1
Rasmus <rasmus.skovhaven@gmail.com>
2022-11-28 11:50:00 +0100

    Added dynamic sheet page

--- pages/sheet/index.vue
+++ pages/sheet/index.vue
@@ -1,1 +1,22 @@
-<template>Silence is golden...</template>
+<script setup>
+const router = useRouter();
+
+function navigate() {
+  return navigateTo({
+    path: "/sheet",
+    query: {
+      id: 1,
+    },
+  });
+}
+
+onMounted(() => {
+  return navigate();
+});
+</script>
+
+<template>
+  <div>
+    {{ $route }}
+  </div>
+</template>
