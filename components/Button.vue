95f587f351eade97f050f78cdbd0a29b3973a0c8
Rasmus <rasmus.skovhaven@gmail.com>
2022-11-29 13:41:51 +0100

    side bar header 'styled' lol

--- components/Button.vue
+++ components/Button.vue
@@ -2,7 +2,8 @@
 const props = defineProps({
   text: {
     type: String,
-    required: true,
+    required: false,
+    default: "",
   },
   icon: {
     type: String,
@@ -24,7 +25,21 @@ const props = defineProps({
     @click="if (props.emit && props.emit.length) $emit(`${props.emit}`);"
     :type="type"
   >
-    {{ props.text }}
+    <slot />
+    <span>{{ props.text }}</span>
     <Icon v-if="props.icon" :icon="props.icon" />
   </button>
 </template>
+
+<style lang="scss" scoped>
+button {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  cursor: pointer;
+}
+svg {
+  height: 25px;
+  width: 25px;
+}
+</style>
