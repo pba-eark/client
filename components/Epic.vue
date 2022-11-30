5d6b1885aadf4e49656cd4e82aae6361afeb764c
Rasmus <rasmus.skovhaven@gmail.com>
2022-11-30 11:05:21 +0100

    Fixed input select options

--- components/Epic.vue
+++ components/Epic.vue
@@ -1,5 +1,4 @@
 <script setup>
-
 import { useAuthStore } from "~/store/auth";
 import { useTaskStore } from "~/store/tasks";
 
@@ -10,31 +9,32 @@ const props = defineProps({
   data: {
     type: Object,
     required: true,
-  }
+  },
 });
 
 const tasks = reactive([]);
 
 onMounted(async () => {
-
   await taskStore.getTasks(authStore.API_TOKEN, props.data.id);
 
-  taskStore.TASKS.forEach(element => {
-    tasks.push(element)
+  taskStore.TASKS.forEach((element) => {
+    tasks.push(element);
   });
-
-})
-
+});
 </script>
 
 <template>
   <div class="block">
     <h2>{{ props.data.epicName }}</h2>
-    <p>{{ props.data.epicStatusId }}</p>
+    <p>{{ props.data.id }}</p>
+
     <Task v-for="task in tasks" :key="task.id" :data="task" />
   </div>
 </template>
 
 <style lang="scss" scoped>
-.block {}
+.block {
+  background: purple;
+  margin: 2rem 0;
+}
 </style>
