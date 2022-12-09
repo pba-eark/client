f745f302293d338c828b5a69d8f125148ea7d7c6
mich5c18 <mich5c18@edu.ucl.dk>
2022-12-09 14:39:26 +0100

    Fixed?

--- store/composites/estimateSheetRiskProfiles.js
+++ store/composites/estimateSheetRiskProfiles.js
@@ -1,10 +1,5 @@
 import { defineStore } from "pinia";
-<<<<<<< HEAD
 import { useAuthStore } from "../auth";
-=======
-import { useAuthStore } from "~~/store/auth";
-import { typeCheck } from "~~/helpers/functions";
->>>>>>> 5eb0cdb (lort)
 
 export const useEstimateSheetRiskProfileStore = defineStore(
   "estimate-sheet-risk-profile-store",
@@ -33,7 +28,6 @@ export const useEstimateSheetRiskProfileStore = defineStore(
         }
       );
 
-<<<<<<< HEAD
       setEstimateSheetRiskProfiles(response);
     };
 
@@ -51,64 +45,6 @@ export const useEstimateSheetRiskProfileStore = defineStore(
             body: obj,
           }
         );
-=======
-  const getEstimateSheetRiskProfiles = async () => {
-    const response = await $fetch(`${runtimeConfig.public.API_URL}/estimatesheetriskprofiles`, {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${authStore.API_TOKEN}`,
-      },
-    });
-    
-    setEstimateSheetRiskProfiles(response);
-  };
-
-  const createEstimateSheetRiskProfile = async (obj) => {
-
-    const response = await $fetch(
-      `${runtimeConfig.public.API_URL}/estimatesheetriskprofiles`,
-      {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${authStore.API_TOKEN}`,
-        },
-        body: obj,
-      });
-
-      estimateSheetRiskProfiles.value = [...estimateSheetRiskProfiles.value, response];
-  };
-
-  const deleteEstimateSheetRiskProfile = async (id) => {
-    await $fetch(
-      `${runtimeConfig.public.API_URL}/estimatesheetriskprofiles/${id}`,
-      {
-        method: "DELETE",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${authStore.API_TOKEN}`,
-        },
-      });
-
-    remove(id);
-  };
-
-  /* Helper functions */
-  const remove = (id) => {
-
-    estimateSheetRiskProfiles.value.forEach((element) => {
-
-      element.id;
-
-      if (element.id === typeCheck(id)) {
-
-        let index = estimateSheetRiskProfiles.value.findIndex(element);
-        estimateSheetRiskProfiles.value.splice(index, 1);
->>>>>>> 5eb0cdb (lort)
 
         estimateSheetRiskProfiles.value = [
           ...estimateSheetRiskProfiles.value,
@@ -135,15 +71,10 @@ export const useEstimateSheetRiskProfileStore = defineStore(
     //   remove(id);
     // };
 
-<<<<<<< HEAD
     // /* Helper functions */
     // const remove = (id) => {
     //   estimateSheetRiskProfiles.value.forEach((element) => {
     //     element.id;
-=======
-  /* Getters */
-  const ESTIMATE_SHEET_RISK_PROFILES = computed(() => estimateSheetRiskProfiles.value);
->>>>>>> 5eb0cdb (lort)
 
     //     if (element.id === typeCheck(id)) {
     //       let index = estimateSheetRiskProfiles.value.findIndex(element);
